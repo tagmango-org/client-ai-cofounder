755f6afffc9f96e2b43df0064fc940778b46ce2b
arjun-fleapo <183461146+arjun-fleapo@users.noreply.github.com>
2025-10-30 19:04:34 +0530

    fix: made the confirmation modal global

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css'
 import Pages from "@/pages/index.tsx"
 import { Toaster } from "@/components/ui/toaster"
+import { ConfirmationModalProvider } from "@/contexts/ConfirmationModalContext"
 
 function App() {
   return (
-    <>
+    <ConfirmationModalProvider>
       <Pages />
       <Toaster />
-    </>
+    </ConfirmationModalProvider>
   )
 }
 
